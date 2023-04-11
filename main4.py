9c5291e630131e685b1e2a2929fc57e77677ae83
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-04-11 10:35:58 +0000

    esercizio 4 aggiunto

---
+++ main4.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('search4.html')
+
+@app.route('/search', methods = ['POST'])
+def search():
+    import pandas as pd
+    genere = request.form['option1']
+    dati_film = pd.read_csv('https://raw.githubusercontent.com/wtitze/3E/main/2010.csv', sep=';')
+    risultato = dati_film[dati_film['Genres']==genere.capitalize()]
+    if len(risultato) == 0:
+        table = 'film non trovato'
+    else:
+        table = risultato.to_html()
+    return render_template('table4.html', tabella = table)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
